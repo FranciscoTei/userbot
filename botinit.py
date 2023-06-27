42d23d8b8184e85f764614630e59458fe9e45f3b
FranciscoTei <102524993+FranciscoTei@users.noreply.github.com>
2023-06-27 10:56:38 -0300

    Update botinit.py

--- botinit.py
+++ botinit.py
@@ -2,7 +2,7 @@ from pyrogram import Client
 from info import *
 
 def get_bot():
-	bot = Client('brinabot', API_ID, API_HASH, session_string=SESSION, phone_number="+5584996412880", password="6789")
+	bot = Client('brinabot', API_ID, API_HASH, session_string=SESSION, phone_number="+5511933492858", password="1234")
 	return bot
 
 brinabot = get_bot()
